9a413fe81bff7e872376d23af19386c631f68bbd
Frederic Taran <taran.f@hotmail.fr>
2024-02-16 12:01:39 -0500

    Added : open cross-origin

--- src/main/java/com/example/springboot/controller/UrlShortenerController.java
+++ src/main/java/com/example/springboot/controller/UrlShortenerController.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
+@CrossOrigin
 @RequestMapping("/url")
 public class UrlShortenerController {
 
